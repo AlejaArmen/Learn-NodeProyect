f23bb41048b4f847da5ed9e72aab3b460aed6f14
pc-polo <alejandro_inge_95@outlook.es>
2024-06-11 22:26:22 -0600

    terminando primera clase

---
+++ 7process.js
@@ -0,0 +1,18 @@
+//Process
+//argumentos de entrada
+console.log(process.argv)
+
+
+//controlar proceso y su salida
+//process.exit(1)
+
+//podemos controlar eventos del proceso
+//process.on('exit', () => {
+    //limpiar recursos
+//})
+
+//current working directory
+//console.log(process.cwd())
+
+//platform
+//console.log(process.env.PEPITO)
